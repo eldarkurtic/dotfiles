791b7df993a63ef7afba3ac36683eaecbdc5061c
Eldar Kurtic <eldar.ciki@gmail.com>
2020-07-27 18:29:39 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-ln -s $HOME/github/dotfiles/zsh .zsh
-ln -s $HOME/github/dotfiles/shell .shell
-ln -s $HOME/github/dotfiles/bash_aliases .bash_aliases
-ln -s $HOME/github/dotfiles/tmux.conf .tmux.conf
-ln -s $HOME/github/dotfiles/vimrc .vimrc
-ln -s $HOME/github/dotfiles/zshrc .zshrc
-ln -s $HOME/github/dotfiles/inputrc .inputrc
+ln -s $HOME/github/dotfiles/zsh $HOME/.zsh
+ln -s $HOME/github/dotfiles/shell $HOME/.shell
+ln -s $HOME/github/dotfiles/bash_aliases $HOME/.bash_aliases
+ln -s $HOME/github/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/github/dotfiles/vimrc $HOME/.vimrc
+ln -s $HOME/github/dotfiles/vim $HOME/.vim
+ln -s $HOME/github/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/github/dotfiles/inputrc $HOME/.inputrc
 ln -s $HOME/github/dotfiles/oh-my-zsh/powerlevel10k $HOME/.oh-my-zsh/themes/powerlevel10k
 ln -s $HOME/github/dotfiles/oh-my-zsh/p10k.zsh $HOME/.p10k.zsh
+
+source $HOME/.bash_aliases
