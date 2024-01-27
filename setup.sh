d4ea92213889cc3a1533239f3acfdbf53924db0c
Eldar Kurtic <eldar.ciki@gmail.com>
2024-01-27 12:11:43 +0100

    cp over ln

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-ln -sf $HOME/github/dotfiles/zsh $HOME/.zsh
-ln -sf $HOME/github/dotfiles/shell $HOME/.shell
-ln -sf $HOME/github/dotfiles/bash_aliases $HOME/.bash_aliases
-ln -sf $HOME/github/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/github/dotfiles/vimrc $HOME/.vimrc
-ln -sf $HOME/github/dotfiles/vim $HOME/.vim
-ln -sf $HOME/github/dotfiles/zshrc $HOME/.zshrc
-ln -sf $HOME/github/dotfiles/inputrc $HOME/.inputrc
-ln -sf $HOME/github/dotfiles/oh-my-zsh/powerlevel10k $HOME/.oh-my-zsh/themes/powerlevel10k
-ln -sf $HOME/github/dotfiles/oh-my-zsh/p10k.zsh $HOME/.p10k.zsh
+cp -r $HOME/github/dotfiles/zsh $HOME/.zsh
+cp -r $HOME/github/dotfiles/shell $HOME/.shell
+cp -r $HOME/github/dotfiles/bash_aliases $HOME/.bash_aliases
+cp -r $HOME/github/dotfiles/tmux.conf $HOME/.tmux.conf
+cp -r $HOME/github/dotfiles/vimrc $HOME/.vimrc
+cp -r $HOME/github/dotfiles/vim $HOME/.vim
+cp -r $HOME/github/dotfiles/zshrc $HOME/.zshrc
+cp -r $HOME/github/dotfiles/inputrc $HOME/.inputrc
+cp -r $HOME/github/dotfiles/oh-my-zsh/powerlevel10k $HOME/.oh-my-zsh/themes/powerlevel10k
+cp -r $HOME/github/dotfiles/oh-my-zsh/p10k.zsh $HOME/.p10k.zsh
 
 source $HOME/.bash_aliases
