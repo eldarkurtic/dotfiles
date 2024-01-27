9d90609b1ddb5c3cf887a6eba47cb8017bee7d02
Eldar Kurtic <eldar.ciki@gmail.com>
2024-01-27 10:52:36 +0100

    force ln

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-ln -s $HOME/github/dotfiles/zsh $HOME/.zsh
-ln -s $HOME/github/dotfiles/shell $HOME/.shell
-ln -s $HOME/github/dotfiles/bash_aliases $HOME/.bash_aliases
-ln -s $HOME/github/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -s $HOME/github/dotfiles/vimrc $HOME/.vimrc
-ln -s $HOME/github/dotfiles/vim $HOME/.vim
-ln -s $HOME/github/dotfiles/zshrc $HOME/.zshrc
-ln -s $HOME/github/dotfiles/inputrc $HOME/.inputrc
-ln -s $HOME/github/dotfiles/oh-my-zsh/powerlevel10k $HOME/.oh-my-zsh/themes/powerlevel10k
-ln -s $HOME/github/dotfiles/oh-my-zsh/p10k.zsh $HOME/.p10k.zsh
+ln -sf $HOME/github/dotfiles/zsh $HOME/.zsh
+ln -sf $HOME/github/dotfiles/shell $HOME/.shell
+ln -sf $HOME/github/dotfiles/bash_aliases $HOME/.bash_aliases
+ln -sf $HOME/github/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/github/dotfiles/vimrc $HOME/.vimrc
+ln -sf $HOME/github/dotfiles/vim $HOME/.vim
+ln -sf $HOME/github/dotfiles/zshrc $HOME/.zshrc
+ln -sf $HOME/github/dotfiles/inputrc $HOME/.inputrc
+ln -sf $HOME/github/dotfiles/oh-my-zsh/powerlevel10k $HOME/.oh-my-zsh/themes/powerlevel10k
+ln -sf $HOME/github/dotfiles/oh-my-zsh/p10k.zsh $HOME/.p10k.zsh
 
 source $HOME/.bash_aliases
